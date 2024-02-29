412c47115c435335ffd967eaad474a56e1a87a3d
sifat26 <sifatict26@gmail.com>
2024-02-29 12:13:40 +0600

    Api added

---
+++ fetch.js
@@ -0,0 +1,11 @@
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+function loaddata() {
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  fetch(url)
+    .then((response) => response.json())
+
+    .then((json) => console.log(json));
+}
